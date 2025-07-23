732311bdc55e614faa424649972a172219f19477
alexanderscottcharles <alexanderscottcharles@gmail.com>
2025-07-23 16:07:36 -0700

    fancy

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,29 +4,55 @@ import React, {useState} from 'react'
 import HamburgerMenu from './HamburgerMenu'
 import Link from 'next/link'
 import Signature from './Signature'
+import Image from 'next/image'
 
 const NavBar = () => {
   const [mobileNav, openMobileNav] = useState(false)
   return (
-    <div>
-        
+    
+        <div>
       <div className="hidden md:block w-full">
-  <div className="flex justify-between items-center px-8"> 
+ <div className="flex justify-between items-start p-8">
 
-    <div className=""> <p className="text-4xl hover:bg-gray-300 rounded-xl"> <Link href="/"><div className="my-8 -mb-1"><Signature /></div></Link></p></div>
-    <div className="flex space-x-8 p-8">
-          <div><p className="text-4xl hover:bg-gray-300 rounded-xl"><Link href="/newsletter" > Newsletter</Link></p></div>
-      <div><p className="text-4xl hover:bg-gray-300 rounded-xl"><Link href="/titles" > Titles</Link></p></div>
-      <div><p className="text-4xl hover:bg-gray-300 rounded-xl"><Link href="/events"> Events</Link></p></div>
-      <div><p className="text-4xl hover:bg-gray-300 rounded-xl"><Link href="/connect">Connect</Link></p></div>
-      <div></div>
+  <Link href="/">
+    <div className="hover:bg-gray-300 rounded-xl">
+      <Signature />
     </div>
+  </Link>
+
+ 
+  <div className="flex items-baseline space-x-8">
+    <Link href="/newsletter">
+      <Image src="/newsletter.png" width={190} height={25} alt="Newsletter" className="hover:bg-gray-300 rounded-xl"/>
+    </Link>
+
+    <Link href="/events">
+      <Image src="/events.png" width={115} height={25} alt="Events" className="hover:bg-gray-300 rounded-xl" />
+    </Link>
+
+    <Link href="/titles">
+      <Image src="/titles.png" width={100} height={25} alt="Titles" className="hover:bg-gray-300 rounded-xl" />
+    </Link>
+
+    <Link href="/connect">
+      <Image src="/socialmedia.png" width={225} height={25} alt="Social Media" className="hover:bg-gray-300 rounded-xl" />
+    </Link>
   </div>
 </div>
+</div>
+
 
 <div className="block w-full md:hidden">
-  <div className="flex justify-between items-center px-4 py-2">
-    <div>ASC</div>
+  <div className="flex justify-between items-center px-4 ">
+<div className="flex items-center h-24">
+  <Image
+    src="/signature-static.png"
+    width={200}
+    height={200}
+    alt="signature"
+    className="hover:bg-gray-300"
+  />
+</div>
     <div className="hover:bg-gray-300" onClick={() => openMobileNav(prev => !prev)}>
       <HamburgerMenu />
     </div>
